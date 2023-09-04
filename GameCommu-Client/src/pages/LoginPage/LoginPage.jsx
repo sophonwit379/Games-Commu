01493c2b1a941ad11fd7cc3fddda9b49b20f7a56
CptSnowball-G <78529993+sophonwit379@users.noreply.github.com>
2023-09-04 18:53:30 +0700

    Initial login page

--- GameCommu-Client/src/pages/LoginPage/LoginPage.jsx
+++ GameCommu-Client/src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,52 @@
+import { useNavigate } from "react-router-dom";
+import { 
+  FloatingLabel,
+  Form,
+  Card,
+  Container,
+  Row,
+  Col,
+  Button 
+} from "react-bootstrap";
+
 
 function LoginPage() {
+  const navigate = useNavigate();
+
+  const login = () => {
+    navigate("/home")
+  }
+
   return (
-        <div>
-          Login
-        </div>
+    <>
+      <Container style={{marginTop:'9rem'}} className='h-50 d-flex justify-content-center'>
+        <Row className="w-50 align-self-center">
+          <Col>
+            <Card>
+              <Card.Body className="mt-3 d-flex flex-column ">
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label="Username"
+                  className="mb-4"
+                >
+                  <Form.Control placeholder="username007" />
+                </FloatingLabel>
+                <FloatingLabel controlId="floatingPassword" label="Password">
+                    <Form.Control type="password" placeholder="Password" />
+                </FloatingLabel>
+                <Button 
+                  className="mt-4 " 
+                  variant="secondary"
+                  onClick={login}
+                  >
+                    Login
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 

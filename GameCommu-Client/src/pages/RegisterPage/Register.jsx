9d54e15c7d256c06028b5157ca53baaa50ba7ac1
CptSnowball-G <78529993+sophonwit379@users.noreply.github.com>
2023-09-05 18:48:38 +0700

    first register page

--- GameCommu-Client/src/pages/RegisterPage/Register.jsx
+++ GameCommu-Client/src/pages/RegisterPage/Register.jsx
@@ -14,7 +14,7 @@ const { Formik } = formik;
 
 const validationSchema = yup.object().shape({
   username: yup.string().required('กรุณากรอกชื่อผู้ใช้งาน'),
-  email: yup.string().email().required('กรุณากรอกอีเมล์'),
+  email: yup.string().email('กรุณากรอกอีเมล์ให่้ถูกต้อง').required('กรุณากรอกอีเมล์'),
   password: yup
     .string()
     .min(8, 'รหัสผ่านต้องมีอย่างน้อย 8 ตัว')
@@ -23,6 +23,7 @@ const validationSchema = yup.object().shape({
     .required('กรุณากรอกรหัสผ่าน'),
   confirmPassword: yup.string()
   .oneOf([yup.ref('password'), null], 'รหัสผ่านไม่ตรงกัน')
+  .required('กรุณากรอกรหัสผ่านอีกครั้ง'),
 });
 
 const initialValues = {
@@ -43,13 +44,73 @@ function Register() {
         initialValues={initialValues}
         onSubmit={onSubmit}       
       >
-        {({handleSubmit,handleChange,values,touched, errors}) => (
-          <Container className='d-flex justify-content-center'>
-            <Row>
-              <Col> 
-                <Card>
+        {({handleSubmit,handleChange,values, errors}) => (
+          <Container style={{minHeight:'75vh'}} className='d-flex justify-content-center'>
+            <Row className='w-50 align-self-center'>
+              <Col className="d-flex flex-column align-items-center"> 
+                <Card className='w-100'>
                   <Card.Body>
-
+                    <Form noValidate onSubmit={handleSubmit} >
+                      <FloatingLabel controlId="username" label="ชื่อผู้ใช้งาน" className="mb-4"
+                      >
+                        <Form.Control 
+                          placeholder="username007"
+                          name="username"
+                          value={values.username}
+                          onChange={handleChange}
+                          isInvalid={!!errors.username}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          {errors.username}
+                        </Form.Control.Feedback>
+                      </FloatingLabel>
+                      <FloatingLabel controlId="email" label="อีเมล์" className="mb-4">
+                          <Form.Control 
+                            type="email" 
+                            placeholder="Email"
+                            name="email"
+                            value={values.email}
+                            onChange={handleChange}
+                            isInvalid={!!errors.email} 
+                          />
+                        <Form.Control.Feedback type="invalid">
+                          {errors.email}
+                        </Form.Control.Feedback>                        
+                      </FloatingLabel>
+                      <FloatingLabel controlId="password" label="รหัสผ่าน" className='mb-4'>
+                          <Form.Control 
+                            type="password" 
+                            placeholder="Password"
+                            name="password"
+                            value={values.password}
+                            onChange={handleChange}
+                            isInvalid={!!errors.password} 
+                          />
+                        <Form.Control.Feedback type="invalid">
+                          {errors.password}
+                        </Form.Control.Feedback>                        
+                      </FloatingLabel>
+                      <FloatingLabel controlId="confirmPassword" label="กรอกรหัสผ่านอีกครั้ง">
+                          <Form.Control 
+                            type="password" 
+                            placeholder="ConfirmPassword"
+                            name="confirmPassword"
+                            value={values.confirmPassword}
+                            onChange={handleChange}
+                            isInvalid={!!errors.confirmPassword} 
+                          />
+                        <Form.Control.Feedback type="invalid">
+                          {errors.confirmPassword}
+                        </Form.Control.Feedback>                        
+                      </FloatingLabel>
+                      <Button 
+                        className="mt-4 w-100" 
+                        variant="secondary"
+                        type="submit"
+                        >
+                          ถัดไป
+                      </Button>
+                    </Form>
                   </Card.Body>
                 </Card>
               </Col>

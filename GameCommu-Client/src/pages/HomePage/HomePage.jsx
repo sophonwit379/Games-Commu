4c27e068915d6f9e6d47319adb84cc0a78036e8a
CptSnowball-G <78529993+sophonwit379@users.noreply.github.com>
2023-09-05 19:02:38 +0700

    fix

--- GameCommu-Client/src/pages/HomePage/HomePage.jsx
+++ GameCommu-Client/src/pages/HomePage/HomePage.jsx
@@ -29,50 +29,50 @@ function HomePage() {
     <div className="all-con">
       <div
         id="carouselExampleControls"
-        class="carousel slide"
+        className="carousel slide"
         data-ride="carousel"
         data-interval="2000"
       >
-        <div class="carousel-inner">
-          <div class="carousel-item active">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
             <img
-              class="d-block w-100 carousel-image"
+              className="d-block w-100 carousel-image"
               src="https://i.ytimg.com/vi/WjVO1CVqu4Y/sddefault.jpg"
               alt="First slide"
             />
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img
-              class="d-block w-100 carousel-image"
+              className="d-block w-100 carousel-image"
               src="https://cdn.cloudflare.steamstatic.com/steam/apps/2280/capsule_616x353.jpg?t=1663861909"
               alt="Second slide"
             />
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img
-              class="d-block w-100 carousel-image"
+              className="d-block w-100 carousel-image"
               src="https://i.ytimg.com/vi/UfP_3UD5qF8/maxresdefault.jpg"
               alt="Third slide"
             />
           </div>
         </div>
         <a
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           href="#carouselExampleControls"
           role="button"
           data-slide="prev"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
         </a>
         <a
-          class="carousel-control-next"
+          className="carousel-control-next"
           href="#carouselExampleControls"
           role="button"
           data-slide="next"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
         </a>
       </div>
       {data.message?.slice(0, visibleItems).map((item, id) => {
